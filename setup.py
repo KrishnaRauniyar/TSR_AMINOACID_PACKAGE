1821f72c6cbeac1f50d135b3f9d36a4eb52dff6f
Krishna Rauniyar <C00551157@louisiana.edu>
2024-11-09 17:05:29 -0600

    amino acid

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aminoacid_tsr_package',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'joblib',
+    ],
+    author='Krishna Rauniyar',
+    author_email='krishna.rauniyar1@louisiana.edu',
+    description='A package for retrieving PDB files and generating key/triplet files for Amino Acid Protein and Mirror, Non-Mirror analysis.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/KrishnaRauniyar/TSR_NUCLEOTIDE_PACKAGE.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
